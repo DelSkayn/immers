a17a1e73bb54568a49763f5d153575fcad1719f0
Mees Delzenne <mees.delzenne@gmail.com>
2020-01-22 15:59:45 +0100

    Export relative idents

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-use immers_core::*;
-use immers_derive::*;
-
-#[derive(Patchable)]
-pub struct Test {
-    foo: u32,
-    bar: i16,
-    baz: Option<u16>,
-}
-
-#[derive(Patchable)]
-pub struct Foo(u32, i32, f32);
+pub use immers_core::*;
+pub use immers_derive::*;
