ffee637f752a4bcc99fc066d6b95bb60e48bdf51
Mees Delzenne <mees.delzenne@gmail.com>
2020-01-22 19:04:25 +0100

    Added option for deriving on generated patches

--- immers_derive/src/lib.rs
+++ immers_derive/src/lib.rs
@@ -3,7 +3,6 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Ident, Index, Member, Visibility};
 
 fn get_name(mut count: u32) -> String {
     let letters = [
@@ -20,7 +19,7 @@ fn get_name(mut count: u32) -> String {
     return res;
 }
 
-fn to_camel_case(ident: &Ident) -> Ident {
+fn to_camel_case(ident: &syn::Ident) -> syn::Ident {
     let text = format!("{}", ident);
     let mut res = String::new();
     res.reserve(text.len());
@@ -39,11 +38,10 @@ fn to_camel_case(ident: &Ident) -> Ident {
             res.push(c);
         }
     }
-    println!("{}", res);
     format_ident!("{}", res)
 }
 
-fn to_snake_case(ident: &Ident) -> Ident {
+fn to_snake_case(ident: &syn::Ident) -> syn::Ident {
     let text = format!("{}", ident);
     let mut res = String::new();
     res.reserve(text.len());
@@ -62,18 +60,22 @@ fn to_snake_case(ident: &Ident) -> Ident {
         }
         first = false;
     }
-    println!("{}", res);
     format_ident!("{}", res)
 }
 
-fn to_tuple_index(count: u32) -> Member {
-    Member::Unnamed(Index {
+fn to_tuple_index(count: u32) -> syn::Member {
+    syn::Member::Unnamed(syn::Index {
         index: count,
         span: Span::call_site(),
     })
 }
 
-fn impl_struct(name: Ident, vis: Visibility, data: DataStruct) -> TokenStream {
+fn impl_struct(
+    name: syn::Ident,
+    vis: syn::Visibility,
+    data: syn::DataStruct,
+    derive: Vec<syn::Ident>,
+) -> TokenStream {
     let mut field_names = Vec::new();
     let mut member_names = Vec::new();
     let mut field_types = Vec::new();
@@ -81,7 +83,7 @@ fn impl_struct(name: Ident, vis: Visibility, data: DataStruct) -> TokenStream {
     for field in data.fields.iter() {
         if let Some(ref x) = field.ident {
             field_names.push(to_camel_case(x));
-            member_names.push(Member::Named(x.clone()));
+            member_names.push(syn::Member::Named(x.clone()));
             field_types.push(field.ty.clone());
         } else {
             let ident = format_ident!("{}", get_name(unnamed_count));
@@ -95,8 +97,12 @@ fn impl_struct(name: Ident, vis: Visibility, data: DataStruct) -> TokenStream {
     let patch_error_name = format_ident!("{}PatchError", name);
     let mod_name = format_ident!("__{}_mod", to_snake_case(&name));
 
+    let derive_tag = quote! {
+        #[derive(#(#derive,)*)]
+    };
+
     let stream = quote! {
-        #[derive(Clone)]
+        #derive_tag
         #vis enum #patch_name {
             #(#field_names (<#field_types as Patchable>::Patch),)*
         }
@@ -147,16 +153,64 @@ fn impl_struct(name: Ident, vis: Visibility, data: DataStruct) -> TokenStream {
             }
         }
     };
-    print!("{}", stream);
     proc_macro::TokenStream::from(stream)
 }
 
-#[proc_macro_derive(Patchable)]
-pub fn derive_patchable(item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as DeriveInput);
+/// Specifically for working with attributes like #[shrinkwrap(..)], where
+/// a name is combined with a list of attributes. Get the list of attributes
+/// matching the tag.
+fn tagged_attrs<'a, A: 'a>(tag: &str, attrs: A) -> Vec<syn::NestedMeta>
+where
+    A: Iterator<Item = &'a syn::Attribute>,
+{
+    use syn::{Meta, MetaList};
 
+    let mut result = vec![];
+
+    for attr in attrs {
+        let meta = attr.parse_meta();
+
+        if let Ok(Meta::List(MetaList { path, nested, .. })) = meta {
+            if path.is_ident(tag) {
+                result.extend(nested);
+            }
+        }
+    }
+
+    result
+}
+
+fn patchable_options(attrs: &[syn::Attribute]) -> Vec<syn::Ident> {
+    use syn::{Meta, MetaList, NestedMeta};
+    let meta = tagged_attrs("patchable", attrs.iter());
+
+    let mut res = vec![format_ident!("Clone")];
+    for m in meta.iter() {
+        if let NestedMeta::Meta(Meta::List(MetaList { path, nested, .. })) = m {
+            if !path.is_ident("derive") {
+                continue;
+            }
+            for d in nested.iter() {
+                if let NestedMeta::Meta(Meta::Path(x)) = d {
+                    x.get_ident().map(|x| {
+                        if x != "Clone" {
+                            res.push(x.clone())
+                        }
+                    });
+                }
+                //TODO handle invalid attrs.
+            }
+        }
+    }
+    res
+}
+
+#[proc_macro_derive(Patchable, attributes(patchable))]
+pub fn derive_patchable(item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::DeriveInput);
+    let derive = patchable_options(&input.attrs);
     match input.data {
-        Data::Struct(x) => impl_struct(input.ident, input.vis, x),
+        syn::Data::Struct(x) => impl_struct(input.ident, input.vis, x, derive),
         _ => todo!(),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
 pub use immers_core::*;
 pub use immers_derive::*;
+
+#[derive(Patchable)]
+#[patchable(derive(Debug, Clone))]
+pub struct Test {
+    a: u16,
+    b: u32,
+    c: u8,
+}
